3714eef5163487df1cd350f219713c401735d3d8
Rima006 <147358423+Rima006@users.noreply.github.com>
2023-12-05 12:06:34 +0800

    Add files via upload
    output inputs

---
+++ App.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.scanner;
+
+public class app {
+
+    public static void main(String[] arges)throws Exception {
+        //deklarasi variabel dan tipe data
+        String nama;
+        String nim;
+        String prodi;
+        Double tinggi badan;
+
+        //membuat scanner baru
+        Scanner input = new scanner(System.in);
+
+        System.out.println("Form Data Mahasiswa UNDIKMA");
+        System.out.println("===========================");
+        //membuat form
+        System.out.print("Masukan Nama Mahasiswa UNDIKMA");
+        nama = input.nextLine();
+
+        System.out.print("Masukan Prodi : ");
+        prodi = input.nexLine();
+        
+        System.out.print("Masukan NIM");.
+        nim = input.nextLine();
+
+        System.out.print("Tinggi Badan : ");
+        tinggi_badan = input.nextDouble();
+
+        //cetak output ke layar monitor
+        System.out.println("Data Mahasiswa UNDIKMA")
+
+    
